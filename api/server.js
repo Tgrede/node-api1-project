10986d8e9de1f94541ac1eea86db9580aeccc3a3
Trenten grede <Trenten.grede@gmail.com>
2021-03-08 20:15:32 -0600

    finished endpoints. application can perform all crud operations

--- api/server.js
+++ api/server.js
@@ -7,7 +7,78 @@ const server = express()
 
 server.use(express.json())
 
+server.get('/api/users', (req, res) => {
+  User.find()
+  .then(users => {
+    res.status(200).json(users)
+  })
+  .catch(err => {
+    res.status(500).json({message: "The users information could not be retrieved"})
+  })
+})
 
+server.get('/api/users/:id', (req, res) => {
+  const id = req.params.id
+  User.findById(id)
+  .then(user => {
+    if(!user){
+      res.status(404).json({message: "The user with the specified ID does not exist"})
+    } else {
+      res.status(200).json(user)
+    }
+  })
+  .catch((err) => {
+    res.status(500).json({message: "The user information could not be retrieved"})
+  })
+})
+
+server.post('/api/users', (req, res) => {
+  const newUser = req.body
+  if(!newUser.name || !newUser.bio){
+    res.status(422).json({message: 'Please provide name and bio for the user'})
+  } else{
+    User.insert(newUser)
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(err => {
+      res.status(500).json({message: "There was an error while saving the user to the database"})
+    })
+  }
+})
+
+server.put('/api/users/:id', async (req, res) => {
+  const {id} = req.params
+  const changes = req.body
+
+  try{
+    if(!changes.name || !changes.bio){
+      res.status(400).json({message: "Please provide name and bio for the user"})
+    } else {
+      const updatedUser = await User.update(id, changes)
+      if (!updatedUser){
+        res.status(404).json({message: 'the user with the specified ID does not exist '})
+        } else {
+          res.status(200).json(updatedUser)
+        }
+      }
+    } catch(err) {
+      res.status(500).json({message:"The user information could not be modified"})
+    }
+})
+
+server.delete('/api/users/:id', async (req, res) => {
+  try{
+    const deleted = await User.remove(req.params.id)
+    if (!deleted){
+      res.status(404).json({message: "there was an issue deleting this resource"})
+    } else {
+      res.json(deleted)
+    }
+  } catch(err) {
+    res.status(404).json({message: err.message})
+  }
+})
 
 server.use('*', (req, res) => {
   res.status(404).json({message: 'resource not found in this server'})
